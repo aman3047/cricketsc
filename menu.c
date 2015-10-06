5c82d89da0d9763fa7db62457baca4fda0c43f8b
aman3047 <amanftadvi@yahoo.co.in>
2015-10-06 23:53:34 +0530

    first commit

---
+++ menu.c
@@ -0,0 +1,9 @@
+char menu() {
+	char ch;
+	printf("\n\t\t\t\tMENU\t\t\t\n");
+	printf("\n   \t1. NEW SCORECARD \t\t\n");
+	printf("\n   \t1. OPEN SCORECARD \t\t\n");
+	printf("\n   \t1. BYE \t\t\n");
+	ch = getchar();
+	return ch;
+}
