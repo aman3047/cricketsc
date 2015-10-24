cb4dc9736ebb3c58b3d0346263d1617d68f0bd4a
aman3047 <amanftadvi@yahoo.co.in>
2015-10-24 16:18:20 +0530

    Delete menu.c

--- menu.c
+++
@@ -1,9 +0,0 @@
-char menu() {
-	char ch;
-	printf("\n\t\t\t\tMENU\t\t\t\n");
-	printf("\n   \t1. NEW SCORECARD \t\t\n");
-	printf("\n   \t1. OPEN SCORECARD \t\t\n");
-	printf("\n   \t1. BYE \t\t\n");
-	ch = getchar();
-	return ch;
-}
