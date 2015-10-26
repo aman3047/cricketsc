e7be0f3f9ec52f3e8fea61aa309a977d3caf3b5a
aman3047 <amanftadvi@yahoo.co.in>
2015-10-26 22:31:05 +0530

    menu.c changed

--- menu.c
+++ menu.c
@@ -1,10 +1,7 @@
+#include <stdio.h>
+#include <main.h>
 char menu() {
 	char ch;
-	WINDOW *d;
-	initscr();
-   	refresh();
-	start_color();
-	init_pair(1,COLOR_BLACK, COLOR_GREEN);
 	move(5, 35);
 	printw("MENU");
 	move(7, 25);
@@ -13,11 +10,6 @@ char menu() {
 	printw("2. OPEN SCORECARD");
 	move(11, 25);
 	printw("3. BYE");
-	bkgd(COLOR_PAIR(1));
-	refresh();
-	getch();
-        delwin(d);
-    	endwin();
 	ch = getchar();
 	return ch;
 }
